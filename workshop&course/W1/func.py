2c2a3dc7e33df5e31d659138b9c17c14869d0a68
Quan Yuan <quanyuan821@gmail.com>
2018-09-02 15:23:58 -0400

    update W1

---
+++ workshop&course/W1/func.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  2 09:11:06 2018
+
+@author: Administrator
+"""
+
+def func1(x):
+    return x
+
---
+++ workshop&course/W1/main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  2 09:11:16 2018
+
+@author: Administrator
+"""
+
+import func as f
+print(f.func1(3))
